434ba084a9f1cb607676f0c0c0e7223d0e8bab0c
R.J. Steinert <rj@rjsteinert.com>
2017-04-26 22:44:29 +0300

    Reorganize components to be more contained in Hero Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { HeroesListComponent } from './heroes/hero.module';
-import { HeroDetailComponent } from './heroes/hero.module';
+import { HeroModule } from './hero/hero.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent, HeroesListComponent, HeroDetailComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
+    HeroModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/hero/hero.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HeroService } from './hero.service';
+import { HeroesListComponent } from './heroes-list/heroes-list.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+
+export {
+    HeroesListComponent
+}
+
+@NgModule({
+    imports: [BrowserModule, FormsModule],
+    exports: [HeroesListComponent],
+    providers: [HeroService],
+    declarations: [ HeroesListComponent, HeroDetailComponent],
+})
+export class HeroModule { }
