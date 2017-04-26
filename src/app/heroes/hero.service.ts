50bd889ff2bb96412b1355029a30df42efc30713
R.J. Steinert <rj@rjsteinert.com>
2017-04-26 16:27:33 +0300

    Add use of await in hero service

--- src/app/heroes/hero.service.ts
+++ src/app/heroes/hero.service.ts
@@ -1,17 +1,26 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero'
 import { HEROES } from './mock-heroes'
+
 @Injectable()
 export class HeroService {
 
     getHeroes(): Promise<Hero[]> {
-        return Promise.resolve(HEROES);
+        return new Promise(function(resolve, reject) {
+            return resolve(HEROES);
+        });
     }
 
     getHeroesSlowly(): Promise<Hero[]> {
-        return new Promise(resolve => {
+        return new Promise((resolve, reject) => {
             setTimeout(() => resolve(this.getHeroes()), 3000);
-        })
+        });
     }
-
+    
+    getHeroesAndFail(): Promise<Hero[]> {
+        return new Promise((resolve, reject) => {
+            reject('Fail');
+            setTimeout(() => resolve(this.getHeroes()), 3000);
+        });
+    }
 }
