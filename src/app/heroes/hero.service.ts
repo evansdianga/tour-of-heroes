16081dfd9cc5fa8dd9dabdd2f0e0955e07b5694e
Evans D <edianga@rti.org>
2017-04-26 16:51:36 +0300

    Service

--- src/app/heroes/hero.service.ts
+++ src/app/heroes/hero.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Hero } from './hero'
-import { HEROES } from './mock-heroes'
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
 @Injectable()
 export class HeroService {
 
@@ -11,7 +11,7 @@ export class HeroService {
     getHeroesSlowly(): Promise<Hero[]> {
         return new Promise(resolve => {
             setTimeout(() => resolve(this.getHeroes()), 3000);
-        })
+        });
     }
 
 }
